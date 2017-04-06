b7aae23288f8761dc29ea578c34d0063cdeb3357
unstad <k_unstad@hotmail.com>
2017-04-06 12:46:02 +0200

    Første commit

---
+++ app/controllers/users.server.controller.js
@@ -0,0 +1,62 @@
+const User = require('mongoose').model('User');
+
+exports.create = function(req, res, next) {
+    const user = new User(req.body);
+
+    user.save((err) => {
+        if (err) {
+            return next (err);
+        }else {
+            res.status(200).json(user);
+        }
+    });
+};
+
+exports.list = function(req, res, next) {
+    User.find({}, (err, users) => {
+        if (err) {
+            return next (err)
+        }else {
+            res.status(200).json(users);
+        }
+    });
+};
+
+exports.read = function(req, res) {
+    res.json(req.user);
+};
+
+exports.userByID = function(req, res, next, id) {
+    User.findOne({
+        _id: id
+    }, (err, user) => {
+        if (err) {
+            return next (err);
+        }else {
+            req.user = user;
+            next();
+        }
+    });
+};
+
+exports.update = function(res, req, next){
+    User.findByIdAndUpdate(req.user.id, req.body, {
+        'new' : true
+    }, (err, user) => {
+        if (err) {
+            return next (err);
+        } else {
+            res.status(200).json(user);
+        }
+    });
+};
+
+exports.delete = function(req, res, next) {
+    req.user.remove(err => {
+        if (err) {
+            return next (err);
+        } else {
+            res.status(200).json(req.user);
+        }
+    })
+};
---
+++ app/models/user.server.model.js
@@ -0,0 +1,98 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    email:{
+        type: String,
+        index: true,
+        match: [/.+\@.+\..+/, "Please fill a valid e-mail address"]
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: 'Username is required',
+        trim: true
+    },
+    password:{
+        type: String,
+        validate:[
+            function(password) {
+                return password.length >=6;
+            },
+            'Password should be longer'
+        ] 
+    },
+    salt: {
+        type: String
+    },
+    provider: {
+        type: String,
+        required: 'Provider is required'
+    },
+    providerId: String,
+    providerData: {},
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    website: {
+        type: String,
+        get: function(url) {
+            if (!url) {
+                return url;
+            } else {
+                if (url.indexOf('http://') !==0 && 
+                url.indexOf('https://') !==0) {
+                    url = 'http://' + url;
+                }
+                return url;
+            }
+        }
+    },
+});
+
+UserSchema.virtual('fullName').get(function() {
+    return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+    const splitName = fullName.split(' ');
+    this.firstName = splitName[0] || " ";
+    this.lastName = splitName[1] || " ";
+});
+
+UserSchema.pre('save', function(next) {
+    if (this.password) {
+        this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+        this.password = this.hashPassword(this.password);
+    }
+    next();
+});
+
+UserSchema.methods.hashPassword = function(password) {
+    return this.password === this.hashPassword(password);
+};
+
+UserSchema.statics.findUniqueUsername = function(username, suffix, callback) {
+    var possibleUsername = username + (suffix || ' ');
+    this.findOne({
+        username: possibleUsername
+    }, (err, user) => {
+        if (!err) {
+            if (!user) {
+                callback(possibleUsername);
+            } else {
+                return this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+            }
+        }else {
+            callback(null);
+        }
+    });
+};
+
+UserSchema.set('toJson', {
+    getters: true,
+    virtuals: true
+});
+
+mongoose.model('User', UserSchema);
