f2fa6a7e67b9652e5f3a69080b1f9c0bd0135fb7
gwib <gwib@users.noreply.github.com>
2017-04-06 14:13:53 +0200

    Connect to db

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const configureMongoose = require('./config/mongoose');
+//const configureMongoose = require('./config/mongoose');
 const configureExpress = require('./config/express');
 const configurePassport = require('./config/passport');
 
-const db = configureMongoose();
+// const db = configureMongoose();
 const app = configureExpress();
 const passport = configurePassport();
 app.listen(3000);
+
+//G: prøver ut noe
+var mongoose = require('mongoose');
+
+mongoose.createConnection('mongodb://heroku_4bpbxhwb:pkvY4zQ1QFBzo4Bfaz95gn9rLP7QOkDz@ds153400.mlab.com:53400/heroku_4bpbxhwb')
+
 
 module.exports = app;
 console.log('Server running at http://localhost:3000/');
