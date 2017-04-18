4375d2580fb942ae32f55de0629ff026be0bed52
unstad <k_unstad@hotmail.com>
2017-04-18 15:49:08 +0200

    autentikasjo vha FB, twitter og google. FB fungerer ikke enda

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -16,4 +16,38 @@ module.exports = function(app) {
         }));
 
     app.get('/signout', users.signout);
+
+//set up the FB OAuth routes
+    //starts the user authenicate process
+    app.get('/oauth/facebook/', passport.authenticate('facebook',{scope: ['email']}, {
+        failureRedirect: '/signin'
+    }));
+
+    //finishes the authenication process once the user har linked their FB profile
+    app.get('/oauth/facebook/callback', passport.authenticate('facebook', {scope: ['email']}, {
+        failureRedirect: '/signin',
+        successRedirect: '/'
+    }));
+
+    app.get('/oauth/twitter', passport.authenticate('twitter', {
+        failureRedirect: '/signin'
+    }));
+
+    app.get('/oauth/twitter/callback', passport.authenticate('twitter', {
+        failureRedirect: '/signin',
+        successRedirect: '/'
+    }));
+
+    app.get('/oauth/google', passport.authenticate('google', {
+        failureRedirect: '/signin',
+        scope: [
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email'
+        ],
+    }));
+
+    app.get('/oauth/google/callback', passport.authenticate('google', {
+        failureRedirect: '/signin',
+        successRedirect: '/'
+    }));
 };
--- config/passport.js
+++ config/passport.js
@@ -16,5 +16,8 @@ module.exports = function() {
             done(err, user);
         });
     });
-    require('./strategies/local.js');
-}
+    require('./strategies/local.js')();
+    require('./strategies/facebook.js')();
+    require('./strategies/twitter.js')();
+    require('./strategies/google.js')();
+};
