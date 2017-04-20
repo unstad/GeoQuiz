41d648495b56fed8745d1ce2289f1ea2151b4fb0
gwib <gwib@users.noreply.github.com>
2017-04-20 13:40:08 +0200

    connection to db. Updated get methods navigating to views

--- app.js
+++ app.js
@@ -7,40 +7,43 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var game = require('./app/routes/game.server.routes');
-var users = require('./app/routes/users.server.routes');
-
-mongoose.Promise = global.Promise;  // try to avoid error
-
-// Connect to db
-mongoose.connect('mongodb://heroku_4bpbxhwb:pkvY4zQ1QFBzo4Bfaz95gn9rLP7QOkDz@ds153400.mlab.com:53400/heroku_4bpbxhwb');
+//var game = require('./app/routes/game.server.routes');
+//var users = require('./app/routes/users.server.routes');
 
 // Databases to be used:
-User = require('./models/user.server/model.js');     // seems wrong hee=!
+//User = require('./models/user.server/model.js');     // seems wrong hee=!
 
 var app = express();
+var router = express.Router();
+
+
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/app/views')));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/app/views'));
 app.set('view engine', 'ejs');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(cookieParser());
 
-app.use('/game', game);
-app.use('/users', users);
+
+
+//app.use('/game', game);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
-});
+});*/
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -52,5 +55,30 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
+// ROUTES
+
+
+app.use("/", router);
+
+router.get("/", function(req, res){
+  console.log("Testing route");
+  //res.sendFile("./app/views/index.ejs");
+  res.render("index.ejs", {root: __dirname + "/app/views" });
+});
+
+router.get("/game", function (req, res) {
+  res.render("game.ejs", {root: __dirname + "/app/views" });
+
+})
+
+mongoose.Promise = global.Promise;  // try to avoid error
+
+// Connect to db
+mongoose.connect('mongodb://heroku_npcxr9bk:ieb5lgo7ejufsm557rnife02s4@ds111851.mlab.com:11851/heroku_npcxr9bk');
+
+
 
 module.exports = app;
