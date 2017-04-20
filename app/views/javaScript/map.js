44c3e9c34cda71dc1617359c7c9e0be4498a9aae
gwib <gwib@users.noreply.github.com>
2017-04-20 14:59:05 +0200

    basic map and necessary changes in stylesheet and game.ejs to display map, working

--- app/views/javaScript/map.js
+++ app/views/javaScript/map.js
@@ -5,14 +5,16 @@
 var map = L.map('mapid').setView(L.latLng(63.42239, 10.42993), 13);
 
 //creating tile layer and adding it to map
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+var tilelayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiZ3ZqIiwiYSI6ImNpeWcwcnhxajAwMWUycW40eWJ3NXdpNGgifQ.73hb0hSrGL22ikJjEfWtJQ'
-}).addTo(map);
+});
+
+map.addLayer(tilelayer);
 
 // doing something when click on map
 function onMapClick(e){
