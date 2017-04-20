d5c0056c4c74b6f8578a2c6be5face28bd347e3e
hephlathio <larsfkjelsaas@gmail.com>
2017-04-20 10:46:07 +0200

    Adjust layout

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/game', game);
