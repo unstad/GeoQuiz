875902c1f4c64624136194af884211dd853bbc29
gwib <gwib@users.noreply.github.com>
2017-04-21 16:20:50 +0200

    modified questionSchema, not sure about its structure yet

--- app/models/questionSchema.js
+++ app/models/questionSchema.js
@@ -2,14 +2,23 @@
  * Created by GalinaJonat on 20/04/17.
  */
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema
+var Schema = mongoose.Schema;
 
 var QuestionSchema = new Schema({
     _id: Number,
     question: String,
     answer_city: String,
     answer: String,
-    geojson_coordinates: geojson
+    geojson_coordinates: {'type': {
+        type: String,
+        required: true,
+    },
+        coordinates: [
+            [
+                {type: [Number]}
+            ]
+        ]
+    }
 });
 
 module.exports = mongoose.model('Question', QuestionSchema);
