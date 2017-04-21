56b1c0f9fab4516762909dd5a1d280d9b8dd7269
unstad <k_unstad@hotmail.com>
2017-04-21 15:19:47 +0200

    uncomment in db

--- app/models/db.js
+++ app/models/db.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'production') {
   dbURI = process.env.MONGOLAB_URI;
 }
 
-mongoose.connect(dbURI);
+//mongoose.connect(dbURI);
 
 // CONNECTION EVENTS
 mongoose.connection.on('connected', function() {
